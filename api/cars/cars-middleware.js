27b89a207f80d28112bd5d8a9a77354b83f231f8
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-08-10 15:50:48 -0500

    middleware wip

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,63 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const { getById, getByVin } = require('./cars-model')
+
+const checkCarId = async (req, res, next) => {
+  const carId = req.params.id
+  const car = await getById(carId)
+
+  if (!car) {
+    res.status(404).send({ message: `car with id ${carId} is not found` })
+  }
+  else {
+    res.locals.car = car
+    next()
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const { vin, make, model, mileage } = req.body
+  let requiredField = undefined
+
+  if (!vin) { requiredField = 'vin' }
+  else if(!make){ requiredField = 'make' }
+  else if(!model) { requiredField = 'model' }
+  else if (!mileage) { requiredField = 'mileage' }
+
+  if (requiredField) {
+    res.status(400).send({ message: `${requiredField} is missing` })
+  }
+  else {
+    next()
+  }
 }
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+  const vin = req.body.vin
+  const invalidVin = vin === undefined || vin.length !== 17
+
+  if (invalidVin) {
+    res.status(400).send({ message: `vin ${vin} is invalid` })
+  }
+  else {
+    next()
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberUnique = async (req, res, next) => {
+  const vin = req.body.vin
+  const carVin = await getByVin(vin)
+  const existingVin = carVin.length > 0
+
+  if (existingVin) {
+    res.status(400).send({ message: `vin ${vin} already exists` })
+  }
+  else {
+    next()
+  }
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
 }
