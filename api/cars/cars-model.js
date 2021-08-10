2eb9e4fc4c3fe1323f2e88b13afd0361434ddbcb
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-08-10 15:33:25 -0500

    model

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,38 @@
-const getAll = () => {
-  // DO YOUR MAGIC
+const knex = require('./../../data/db-config')
+
+const getAll = async () => {
+  const cars = await knex
+    .from('cars')
+    .select('id', 'vin', 'make', 'model', 'mileage', 'title', 'transmission')
+  return cars
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = async (id) => {
+  const carId = await knex
+    .from('cars')
+    .where({ id: id })
+    .select('id', 'vin', 'make', 'model', 'mileage', 'title', 'transmission')
+  return carId[0]
 }
 
-const create = () => {
-  // DO YOUR MAGIC
+const getByVin = async (vin) => {
+  const carVin = knex
+    .from('cars')
+    .where({ vin })
+    .select('id')
+  return carVin
 }
+
+const create = ({ vin, make, model, mileage, title, transmission }) => {
+  const insertResult = knex('cars')
+    .insert({ vin, make, model, mileage, title, transmission })
+  return insertResult
+}
+
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+  getByVin
+} 
